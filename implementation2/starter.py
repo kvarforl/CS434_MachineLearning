780aa0a4e02a48cdcca342ed8c7487299ba12a31
Lindsey Kvarfordt <kvarforl@oregonstate.edu>
2020-04-22 12:53:22 -0700

    added p(wi | y) function

--- implementation2/starter.py
+++ implementation2/starter.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 import re
+import numpy as np
 
 # Importing the dataset
 imdb_data = pd.read_csv('IMDB.csv', delimiter=',')
@@ -39,28 +40,30 @@ def clean_text(text):
 
     return text
 
+#takes a matrix of class y reviews, where each review is a row, and each column is a count of word occurances
+#takes an alpha value for smoothing; defaults to 1
+def p_wi_given_y(features, alpha=1):
+    numerator = np.sum(features, axis=0) + alpha #vector of word counts in features matrix + alpha
+    denominator = np.sum(features) + (features.shape[1] * alpha)  #total number of words in class + |V|alpha
+    return (1/denominator) * numerator
 
 # this vectorizer will skip stop words
 vectorizer = CountVectorizer(
     stop_words="english",
-    preprocessor=clean_text
+    preprocessor=clean_text,
+    max_features=2000,
 )
 
 # fit the vectorizer on the text
 features = vectorizer.fit_transform(imdb_data['review'])
 
 # Split feature vectors into testing and validation
-train_features = labelArray[0:30000, :]
-validation_features = labelArray[30000:40000 , :]
+train_features = features[0:30000, :]
+validation_features = features[30000:40000 , :]
 
 # get the vocabulary
 inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
 vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
 
-# Apply settings for part 1
-vectorizer.max_features = 2000
-
-
-# Train classifier
 
 
