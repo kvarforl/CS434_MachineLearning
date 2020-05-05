fc04ba39a82546c3d61bbe4c180e43de712aa7ce
Lindsey Kvarfordt <kvarforl@oregonstate.edu>
2020-05-05 12:00:50 -0700

    added plots for part 1

--- implementation3/src/main.py
+++ implementation3/src/main.py
@@ -39,6 +39,35 @@ def decision_tree_testing(x_train, y_train, x_test, y_test):
 	preds = clf.predict(x_test)
 	print('F1 Test {}'.format(f1(y_test, preds)))
 
+def test_depth(x_train, y_train, x_test, y_test):
+	print("Decision Tree Depth Test")
+	depths = list(range(1, 26)) #1 through 25, inclusive
+	test_accuracies = []
+	train_accuracies = []
+	f1_scores = []
+	for d in depths:
+		clf = DecisionTreeClassifier(max_depth=d)
+		clf.fit(x_train, y_train)
+		preds_train = clf.predict(x_train)
+		preds_test = clf.predict(x_test)
+		train_accuracies.append(accuracy_score(preds_train, y_train))
+		test_accuracies.append(accuracy_score(preds_test, y_test))
+		preds = clf.predict(x_test)
+		f1_scores.append(f1(y_test, preds))
+	print("done computing")
+	fig, ax = plt.subplots()
+	ax.plot(depths, test_accuracies)
+	ax.plot(depths, train_accuracies)
+	ax.set(xlabel="depth of decision tree", ylabel="accuracy")
+	fig.legend(["Testing", "Train"])
+	fig.savefig("accuracies_v_tree_depth.png")	
+
+	fig, ax = plt.subplots()
+	ax.plot(depths, f1_scores)
+	ax.set(xlabel="depth of decision tree", ylabel="f1 score")
+	fig.savefig("f1scores_v_tree_depth.png")	
+
+
 def random_forest_testing(x_train, y_train, x_test, y_test):
 	print('Random Forest\n\n')
 	rclf = RandomForestClassifier(max_depth=7, max_features=11, n_trees=50)
@@ -54,6 +83,7 @@ def random_forest_testing(x_train, y_train, x_test, y_test):
 
 
 
+
 ###################################################
 # Modify for running your experiments accordingly #
 ###################################################
@@ -64,6 +94,7 @@ if __name__ == '__main__':
 		county_info(args)
 	if args.decision_tree == 1:
 		decision_tree_testing(x_train, y_train, x_test, y_test)
+		test_depth(x_train, y_train, x_test, y_test)
 	if args.random_forest == 1:
 		random_forest_testing(x_train, y_train, x_test, y_test)
 
